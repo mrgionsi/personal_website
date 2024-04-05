2f1b07857ecdab24d54626812bb40700149c8392
Giovanni Pasquariello <g.pasquariello96@gmail.com>
2024-04-05 16:46:41 +0200

    Updated to angular 17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,6 @@ import { SkillPageComponent } from './skill-page/skill-page.component';
 import { EducationPageComponent } from './education-page/education-page.component';
 import { ExperiencePageComponent } from './experience-page/experience-page.component';
 import { WorkexperienceTimelineComponent } from './experience-page/workexperience-timeline/workexperience-timeline.component';
-import { NgVerticalTimelineModule } from 'ng-vertical-timeline';
 import { PersonalWorkPageComponent } from './personal-work-page/personal-work-page.component';
 import { ContactPageComponent } from './contact-page/contact-page.component';
 
@@ -22,12 +21,12 @@ import { ContactPageComponent } from './contact-page/contact-page.component';
     SkillPageComponent,
     EducationPageComponent,
     ExperiencePageComponent,
+    
     WorkexperienceTimelineComponent,
     PersonalWorkPageComponent,
     ContactPageComponent
   ],
   imports: [
-    NgVerticalTimelineModule,
     BrowserModule,
     AppRoutingModule,
     NgbModule
