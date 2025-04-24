41e1bf352930a2b02c91fd4bf4101ae99c4aa104
Giovanni Pasquariello <g.pasquariello96@gmail.com>
2025-04-24 12:54:31 +0200

    Set backround white on scroll

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
 export class MenuComponent {
+  scrolled = false;
 
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    // Check if the window has been scrolled down by 50px or more
+    this.scrolled = window.scrollY > 50;
+  }
 }
